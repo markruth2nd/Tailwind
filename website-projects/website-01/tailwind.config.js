c4cae96c923773487167711d06e863ce19172734
Mark Baldry <markbaldry@harrisoncarloss.com>
2024-02-26 21:01:57 +0000

    Loopstudios 1 page website added

--- website-projects/website-01/tailwind.config.js
+++ website-projects/website-01/tailwind.config.js
@@ -1,23 +1,7 @@
 module.exports = {
-  content: ["./*.html"],
+  content: ['./*.html'],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    extend: {
-      colors: {
-        strongCyan: "hsl(171, 66%, 44%)",
-        lightBlue: "hsl(233, 100%, 69%)",
-        darkGrayishBlue: "hsl(210, 11%, 66%)",
-        grayishBlue: "hsl(201, 11%, 66%)",
-      },
-      fontFamily: {
-        sans: ["Bai Jamjuree", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
